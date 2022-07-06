1a9f122b06e6ce135af9e3d91ab543dbcd60301c
patdz <patdz@ymipro.com>
2022-07-06 14:32:25 +0800

    initial code

---
+++ grpce/meta/meta.go
@@ -0,0 +1,149 @@
+package meta
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math/rand"
+	"strconv"
+
+	"google.golang.org/grpc/metadata"
+)
+
+const (
+	// RequestIdOnMetaData unique request id
+	RequestIdOnMetaData = "ymi-micro-srv-req-id"
+)
+
+func randomNumber() uint64 {
+	return uint64(rand.Int63())
+}
+
+func getRandomID() string {
+	return fmt.Sprintf("%x", randomNumber())
+}
+
+func GetRequestIDFromMD(md metadata.MD) string {
+	ids := md.Get(RequestIdOnMetaData)
+	if len(ids) > 0 {
+		return ids[0]
+	}
+	return ""
+}
+
+/*
+func IdFromIncomingContext(ctx context.Context) (id string, newCreateFlag bool) {
+	if md, ok := metadata.FromIncomingContext(ctx); ok {
+		id = GetRequestIDFromMD(md)
+	}
+
+	if id == "" {
+		id = getRandomID()
+		newCreateFlag = true
+	}
+	return
+}
+
+func IdToOutgoingContext(ctx context.Context, id string) context.Context {
+	return metadata.AppendToOutgoingContext(ctx, RequestIdOnMetaData, id)
+}
+*/
+
+func IdFromOutgoingContext(ctx context.Context) string {
+	if md, ok := metadata.FromOutgoingContext(ctx); ok {
+		return GetRequestIDFromMD(md)
+	}
+	return ""
+}
+
+func TransferContextMeta(ctx context.Context, keys []string) context.Context {
+	var idInIncomingContext, idInOutgoingContext string
+
+	mdIn, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		mdIn = metadata.New(nil)
+	}
+
+	for _, v := range mdIn.Get(RequestIdOnMetaData) {
+		if v != "" {
+			idInOutgoingContext = v
+			break
+		}
+	}
+
+	mdOut := metadata.New(nil)
+	if md, ok := metadata.FromOutgoingContext(ctx); ok {
+		for key, vs := range md {
+			if key == RequestIdOnMetaData {
+				for _, v := range vs {
+					if v != "" {
+						idInOutgoingContext = v
+						break
+					}
+				}
+				continue
+			}
+			mdOut.Set(key, vs...)
+		}
+	}
+
+	if keys == nil {
+		keys = make([]string, 0, mdIn.Len())
+		for key := range mdIn {
+			if key == RequestIdOnMetaData {
+				continue
+			}
+			keys = append(keys, key)
+		}
+	}
+
+	if idInIncomingContext == "" {
+		idInIncomingContext = idInOutgoingContext
+	}
+	if idInIncomingContext == "" {
+		idInIncomingContext = getRandomID()
+	}
+
+	for _, key := range keys {
+		if key == RequestIdOnMetaData {
+			continue
+		}
+		if len(mdIn[key]) == 0 {
+			continue
+		}
+		if len(mdOut[key]) > 0 {
+			continue
+		}
+		mdOut.Set(key, mdIn[key]...)
+	}
+	mdOut.Set(RequestIdOnMetaData, idInIncomingContext)
+
+	return metadata.NewOutgoingContext(ctx, mdOut)
+}
+
+func GetFromMetaString(ctx context.Context, key string) (string, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if ok {
+		values := md.Get(key)
+		if len(values) > 0 {
+			return values[0], nil
+		}
+	}
+	return "", errors.New("not exists")
+}
+
+func GetStringFromMeta(ctx context.Context, key string) (string, error) {
+	return GetFromMetaString(ctx, key)
+}
+
+func GetIntFromMeta(ctx context.Context, key string) (int64, error) {
+	v, err := GetStringFromMeta(ctx, key)
+	if err != nil {
+		return 0, err
+	}
+	vv, err := strconv.ParseInt(v, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	return vv, nil
+}
---
+++ servicetoolset/servcie_toolset.go
@@ -0,0 +1,83 @@
+package servicetoolset
+
+import (
+	"context"
+	"strings"
+
+	"github.com/sgostarter/i/l"
+	"github.com/sgostarter/libeasygo/commerr"
+	"go.uber.org/atomic"
+	"google.golang.org/grpc"
+)
+
+type ServerToolset struct {
+	ctx          context.Context
+	logger       l.Wrapper
+	serverHelper *ServerHelper
+	gRpcServer   GRPCServer
+	httpServer   HTTPServer
+
+	started atomic.Bool
+}
+
+func NewServerToolset(ctx context.Context, logger l.Wrapper) *ServerToolset {
+	if logger == nil {
+		logger = l.NewNopLoggerWrapper()
+	}
+
+	sst := &ServerToolset{
+		ctx:    ctx,
+		logger: logger.WithFields(l.StringField(l.ClsKey, "ServerToolset")),
+	}
+
+	sst.serverHelper = NewServerHelper(ctx, logger)
+	return sst
+}
+
+func (st *ServerToolset) CreateGRpcServer(cfg *GRPCServerConfig, opts []grpc.ServerOption, beforeServerStart BeforeServerStart) (err error) {
+	if st.gRpcServer != nil {
+		err = commerr.ErrAlreadyExists
+
+		return
+	}
+
+	st.gRpcServer, err = NewGRPCServer(nil, cfg, opts, beforeServerStart, st.logger)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (st *ServerToolset) CreateHttpServer(cfg *HttpServerConfig) error {
+	if st.httpServer != nil {
+		return commerr.ErrAlreadyExists
+	}
+
+	if cfg == nil || cfg.Address == "" || !strings.Contains(cfg.Address, ":") || cfg.Handler == nil {
+		return commerr.ErrInvalidArgument
+	}
+	st.httpServer = NewHTTPServer(cfg.Address, st.logger, cfg.Handler)
+
+	return nil
+}
+
+func (st *ServerToolset) Start() error {
+	if !st.started.CAS(false, true) {
+		return commerr.ErrAlreadyExists
+	}
+
+	if st.gRpcServer != nil {
+		st.serverHelper.StartServer(st.gRpcServer)
+	}
+	if st.httpServer != nil {
+		st.serverHelper.StartServer(st.httpServer)
+	}
+
+	return nil
+}
+
+func (st *ServerToolset) Wait() {
+	_ = st.Start()
+	st.serverHelper.Wait()
+}
