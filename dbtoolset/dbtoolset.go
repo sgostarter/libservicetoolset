571739c3e92ea3234b6c3f823d571f0105dd34f5
patdz <patdz@ymipro.com>
2022-07-13 16:07:23 +0800

    dbtoolset

---
+++ dbtoolset/dbtoolset.go
@@ -0,0 +1,189 @@
+package dbtoolset
+
+import (
+	"strings"
+	"sync"
+
+	// make sure mysql engine
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/sgostarter/i/l"
+	"github.com/sgostarter/libeasygo/stg"
+	"gorm.io/gorm"
+	"xorm.io/xorm"
+)
+
+const (
+	DefaultName = "default"
+)
+
+type Config struct {
+	RedisDSN     string            `yaml:"redis_dsn" json:"redis_dsn"`
+	RedisDSNList map[string]string `yaml:"redis_dsn_list" json:"redis_dsn_list"`
+
+	MysqlDSN     string            `yaml:"mysql_dsn" json:"mysql_dsn"`
+	MysqlDSNList map[string]string `yaml:"mysql_dsn_list" json:"mysql_dsn_list"`
+}
+
+type Toolset struct {
+	cfg    *Config
+	logger l.Wrapper
+
+	redisOnce sync.Once
+	redisCli  *redis.Client
+
+	redisListOnce sync.Once
+	redisList     map[string]*redis.Client
+
+	xOrmOnce   sync.Once
+	xOrmEngine *xorm.Engine
+
+	xOrmListOnce sync.Once
+	xOrmList     map[string]*xorm.Engine
+
+	gOrmOnce sync.Once
+	gOrmDb   *gorm.DB
+
+	gOrmListOnce sync.Once
+	gOrmList     map[string]*gorm.DB
+}
+
+func NewToolset(cfg *Config, logger l.Wrapper) *Toolset {
+	if logger == nil {
+		logger = l.NewNopLoggerWrapper()
+	}
+
+	return &Toolset{
+		cfg:       cfg,
+		logger:    logger.WithFields(l.StringField(l.ClsKey, "Toolset")),
+		redisList: make(map[string]*redis.Client),
+		xOrmList:  make(map[string]*xorm.Engine),
+		gOrmList:  make(map[string]*gorm.DB),
+	}
+}
+
+func (toolset *Toolset) GetRedis() *redis.Client {
+	toolset.redisOnce.Do(func() {
+		redisCli, err := stg.InitRedis(toolset.cfg.RedisDSN)
+		if err != nil {
+			toolset.logger.WithFields(l.ErrorField(err), l.StringField("dsn", toolset.cfg.RedisDSN)).Error("initRedisFailed")
+
+			return
+		}
+
+		toolset.redisCli = redisCli
+	})
+
+	return toolset.redisCli
+}
+
+func (toolset *Toolset) GetRedisByName(name string) *redis.Client {
+	toolset.redisListOnce.Do(func() {
+		for name, dsn := range toolset.cfg.RedisDSNList {
+			redisCli, err := stg.InitRedis(dsn)
+			if err != nil {
+				toolset.logger.WithFields(l.ErrorField(err), l.StringField("name", name),
+					l.StringField("dsn", toolset.cfg.RedisDSN)).Error("initRedisFailed")
+
+				return
+			}
+
+			toolset.redisList[name] = redisCli
+		}
+	})
+
+	if redisCli, ok := toolset.redisList[name]; ok {
+		return redisCli
+	}
+
+	if strings.EqualFold(name, DefaultName) {
+		return toolset.GetRedis()
+	}
+
+	return nil
+}
+
+func (toolset *Toolset) GetXOrm() *xorm.Engine {
+	toolset.xOrmOnce.Do(func() {
+		db, err := xorm.NewEngine("mysql", toolset.cfg.MysqlDSN)
+		if err != nil {
+			toolset.logger.WithFields(l.ErrorField(err), l.StringField("dsn", toolset.cfg.MysqlDSN)).
+				Error("initXOrmFailed")
+
+			return
+		}
+
+		toolset.xOrmEngine = db
+	})
+
+	return toolset.xOrmEngine
+}
+
+func (toolset *Toolset) GetXOrmByName(name string) *xorm.Engine {
+	toolset.xOrmListOnce.Do(func() {
+		for name, dsn := range toolset.cfg.MysqlDSNList {
+			db, err := xorm.NewEngine("mysql", dsn)
+			if err != nil {
+				toolset.logger.WithFields(l.ErrorField(err), l.StringField("dsn", toolset.cfg.MysqlDSN),
+					l.StringField("name", name)).Error("initXOrmFailed")
+
+				return
+			}
+
+			toolset.xOrmList[name] = db
+		}
+	})
+
+	if db, ok := toolset.xOrmList[name]; ok {
+		return db
+	}
+
+	if strings.EqualFold(name, DefaultName) {
+		return toolset.GetXOrm()
+	}
+
+	return nil
+}
+
+func (toolset *Toolset) GetGOrm() *gorm.DB {
+	toolset.gOrmOnce.Do(func() {
+		db, err := stg.InitGorm(toolset.cfg.MysqlDSN)
+		if err != nil {
+			toolset.logger.WithFields(l.ErrorField(err), l.StringField("dsn", toolset.cfg.MysqlDSN)).
+				Error("initGOrmFailed")
+
+			return
+		}
+
+		toolset.gOrmDb = db
+	})
+
+	return toolset.gOrmDb
+}
+
+func (toolset *Toolset) GetGOrmByName(name string) *gorm.DB {
+	toolset.gOrmListOnce.Do(func() {
+		for name, dsn := range toolset.cfg.MysqlDSNList {
+			db, err := stg.InitGorm(dsn)
+			if err != nil {
+				toolset.logger.WithFields(l.ErrorField(err), l.StringField("dsn", dsn),
+					l.StringField("name", name)).Error("initGOrmFailed")
+
+				return
+			}
+
+			toolset.gOrmList[name] = db
+		}
+	})
+
+	if db, ok := toolset.gOrmList[name]; ok {
+		return db
+	}
+
+	if strings.EqualFold(name, DefaultName) {
+		return toolset.GetGOrm()
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/sgostarter/libservicetoolset
 go 1.17
 
 require (
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
 	github.com/improbable-eng/grpc-web v0.15.0
 	github.com/sgostarter/i v0.1.11
@@ -12,6 +13,7 @@ require (
 	go.uber.org/atomic v1.9.0
 	google.golang.org/grpc v1.47.0
 	google.golang.org/grpc/examples v0.0.0-20220701230310-8c494a9aed3e
+	xorm.io/xorm v1.3.1
 )
 
 require (
@@ -20,18 +22,23 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/desertbit/timer v0.0.0-20180107155436-c41aec40b27f // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/goccy/go-json v0.8.1 // indirect
 	github.com/godruoyi/go-snowflake v0.0.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.3 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kelseyhightower/envconfig v1.4.0 // indirect
 	github.com/klauspost/compress v1.11.7 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rs/cors v1.7.0 // indirect
 	github.com/sgostarter/libconfig v0.0.0-20220501124634-dd2bd2401e61 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/sys v0.0.0-20220328115105-d36c6a25d886 // indirect
 	golang.org/x/text v0.3.7 // indirect
@@ -41,4 +48,5 @@ require (
 	gorm.io/driver/mysql v1.2.1 // indirect
 	gorm.io/gorm v1.22.4 // indirect
 	nhooyr.io/websocket v1.8.6 // indirect
+	xorm.io/builder v0.3.11-0.20220531020008-1bd24a7dc978 // indirect
 )
